9d7fdcfe3823a65037d73ccd23dd939c283b8274
ddrhckrzz <andreimartillo2000@gmail.com>
2024-05-01 18:20:29 +0800

    added some comments

--- src/up/tac/cmsc12/mp/ui/frames/GamePanel.java
+++ src/up/tac/cmsc12/mp/ui/frames/GamePanel.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 import up.tac.cmsc12.mp.ui.buttons.Cells;
 
 public class GamePanel extends JPanel {
+    // TODO: finish logic and stuff.
     private static final int DEFAULT_ROWS = 9;
     private static final int DEFAULT_COLS = 9;
     private static final int DEFAULT_NO_OF_MINES = 10;
@@ -20,6 +21,12 @@ public class GamePanel extends JPanel {
     private JPanel boardPanel;
     private JPanel bottomPanel;
 
+    /**
+     * Default constructor that uses default board size
+     * of 9x9 and only having 10 mines total.
+     * 
+     * Currently not fully implemented yet.
+     */
     public GamePanel(){
         rows = DEFAULT_ROWS;
         cols = DEFAULT_COLS;
