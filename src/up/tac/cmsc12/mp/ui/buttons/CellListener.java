0fec7de8f72e0701ca309445e3b7cc07367b0c7d
ddrhckrzz <andreimartillo2000@gmail.com>
2024-05-01 18:00:23 +0800

    added default board button generation and initially created the CellListener for mouse events

---
+++ src/up/tac/cmsc12/mp/ui/buttons/CellListener.java
@@ -0,0 +1,12 @@
+package up.tac.cmsc12.mp.ui.buttons;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class CellListener extends MouseAdapter{
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+        
+    }
+}
--- src/up/tac/cmsc12/mp/ui/frames/GamePanel.java
+++ src/up/tac/cmsc12/mp/ui/frames/GamePanel.java
@@ -1,5 +1,9 @@
 package up.tac.cmsc12.mp.ui.frames;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import up.tac.cmsc12.mp.ui.buttons.Cells;
@@ -10,11 +14,52 @@ public class GamePanel extends JPanel {
     private static final int DEFAULT_NO_OF_MINES = 10;
     private int rows;
     private int cols;
+    private int totalMines;
     private Cells[][] board;
+    private JPanel topPanel;
+    private JPanel boardPanel;
+    private JPanel bottomPanel;
 
     public GamePanel(){
         rows = DEFAULT_ROWS;
         cols = DEFAULT_COLS;
+        totalMines = DEFAULT_NO_OF_MINES;
         board = new Cells[rows][cols];
+        init_layout();
+    }
+
+    private void init_layout(){
+        setLayout(new BorderLayout());
+        init_topPanel();
+        add(topPanel, BorderLayout.NORTH);
+        init_board();
+        add(boardPanel, BorderLayout.CENTER);
+        init_bottomPanel();
+        add(bottomPanel, BorderLayout.SOUTH);
+    }
+
+    private void init_topPanel(){
+        topPanel = new JPanel();
+        JLabel timer = new JLabel("timer");
+        timer.setHorizontalAlignment(JLabel.CENTER);
+        topPanel.add(timer);
+    }
+
+    private void init_bottomPanel(){
+        bottomPanel = new JPanel();
+        JLabel minesLeft = new JLabel("Mines Left: ");
+        minesLeft.setHorizontalAlignment(JLabel.CENTER);
+        bottomPanel.add(minesLeft);
+    }
+
+    private void init_board(){
+        boardPanel = new JPanel();
+        boardPanel.setLayout(new GridLayout(rows, cols));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = new Cells();
+                boardPanel.add(board[i][j]);
+            }
+        }
     }
 }
