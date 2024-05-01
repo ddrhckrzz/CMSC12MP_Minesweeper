ac59cc68ccd71d0ef654bf7c3f5c7bc7b9e02956
ddrhckrzz <andreimartillo2000@gmail.com>
2024-05-01 17:20:36 +0800

    created GamePanel default constructor and constants

--- src/up/tac/cmsc12/mp/ui/frames/GamePanel.java
+++ src/up/tac/cmsc12/mp/ui/frames/GamePanel.java
@@ -2,6 +2,19 @@ package up.tac.cmsc12.mp.ui.frames;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+import up.tac.cmsc12.mp.ui.buttons.Cells;
 
+public class GamePanel extends JPanel {
+    private static final int DEFAULT_ROWS = 9;
+    private static final int DEFAULT_COLS = 9;
+    private static final int DEFAULT_NO_OF_MINES = 10;
+    private int rows;
+    private int cols;
+    private Cells[][] board;
+
+    public GamePanel(){
+        rows = DEFAULT_ROWS;
+        cols = DEFAULT_COLS;
+        board = new Cells[rows][cols];
+    }
 }
