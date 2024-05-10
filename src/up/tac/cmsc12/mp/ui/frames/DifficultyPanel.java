af251adde651fbd6b3b9770e390865c31bd0651f
ddrhckrzz <andreimartillo2000@gmail.com>
2024-05-10 23:01:21 +0800

    did some initial layout code for DifficultyPanel. not tested yet.

--- src/up/tac/cmsc12/mp/ui/frames/DifficultyPanel.java
+++ src/up/tac/cmsc12/mp/ui/frames/DifficultyPanel.java
@@ -1,8 +1,149 @@
 package up.tac.cmsc12.mp.ui.frames;
 
-public class DifficultyPanel {
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class DifficultyPanel extends JPanel {
+    public static final String DIFFICULTY_PANEL = "DIFFICULTY";
+    public static final String CUSTOM_DIFFICULTY = "CUSTOM DIFFICULTY";
+    private CardLayout cardLayout;
+    private JPanel difficultyPanel;
+    private JPanel customDifficultyPanel;
+    private JTextField rowsField;
+    private JTextField colsField;
+    private JTextField totalMinesField;
     private GamePanel gamePanel;
     public DifficultyPanel(GamePanel gamePanel){
         this.gamePanel = gamePanel;
+        init_layout();
+    }
+
+    private void init_layout(){
+        cardLayout = new CardLayout();
+        setLayout(cardLayout);
+        difficultyPanel = makeDifficultyPanel();
+        cardLayout.addLayoutComponent(difficultyPanel, DIFFICULTY_PANEL);
+        customDifficultyPanel = makeCustomDifficultyPanel();
+        cardLayout.addLayoutComponent(customDifficultyPanel, CUSTOM_DIFFICULTY);
+    }
+
+    private JPanel makeDifficultyPanel(){
+        // panels within panels within panels holy f*ck
+        JPanel buttonsPanel = new JPanel();
+        JPanel wrapper = new JPanel();
+        JPanel topPanel = new JPanel();
+        wrapper.setLayout(new BorderLayout());
+        buttonsPanel.setLayout(new GridBagLayout());
+        buttonsPanel.setMaximumSize(new Dimension(360, 510));
+        JButton back = new JButton("Back");
+        JButton home = new JButton("Main Menu");
+        // the home button and back button on the top left
+        topPanel.add(back);
+        topPanel.add(home);
+        wrapper.add(topPanel, BorderLayout.NORTH);
+        // making the different buttons
+        JButton beginner = new JButton("BEGINNER");
+        JButton intermediate = new JButton("INTERMEDIATE");
+        JButton expert = new JButton("EXPERT");
+        JButton master = new JButton("MASTER");
+        JButton legend = new JButton("LEGEND");
+        JButton custom = new JButton("CUSTOM");
+        // TODO: add actonlisteners to all of these
+
+        // adding buttons to the button panel
+        // GridBagConstraints is amazing but man...
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(12, 12, 12, 12);
+        c.fill = GridBagConstraints.BOTH;
+        setGBC(c, 1.0, 1.0);
+        buttonsPanel.add(beginner, c);
+        buttonsPanel.add(intermediate, c);
+        buttonsPanel.add(expert, c);
+        buttonsPanel.add(master, c);
+        buttonsPanel.add(legend, c);
+        buttonsPanel.add(custom, c);
+        // finally...
+        wrapper.add(buttonsPanel, BorderLayout.CENTER);
+        return wrapper;
+    }
+
+    private JPanel makeCustomDifficultyPanel(){
+        // even more panels within panels shenanigans
+        JPanel buttonsPanel = new JPanel();
+        JPanel wrapper = new JPanel();
+        JPanel topPanel = new JPanel();
+        wrapper.setLayout(new BorderLayout());
+        buttonsPanel.setLayout(new GridBagLayout());
+        buttonsPanel.setMaximumSize(new Dimension(360, 510));
+        JButton back = new JButton("Back");
+        JButton home = new JButton("Main Menu");
+        // the home button and back button on the top left
+        topPanel.add(back);
+        topPanel.add(home);
+        wrapper.add(topPanel, BorderLayout.NORTH);
+        // gridbag stuff again wowee
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        setGBC(c, 1.0, 1.0);
+        JLabel topLabel = new JLabel("CUSTOM GAME SETUP");
+        JLabel rowsLabel = new JLabel("No. of Rows (max: 250): ");
+        JLabel colsLabel = new JLabel("No. of Columns (max: 250): ");
+        JLabel totalMinesLabel = new JLabel("No. of Mines (max: (rows * cols) - 1): ");
+        rowsField = new JTextField(10);
+        colsField = new JTextField(10);
+        totalMinesField = new JTextField(10);
+        c.gridwidth = 2;
+        c.gridheight = 2;
+        setGBC(c, 0, 0);
+        buttonsPanel.add(topLabel, c);
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        setGBC(c, 0, 1, 0.5, 0.5);
+        buttonsPanel.add(rowsLabel,c);
+        setGBC(c, 1, 1);
+        buttonsPanel.add(rowsField, c);
+        setGBC(c, 0, 2);
+        buttonsPanel.add(colsLabel, c);
+        setGBC(c, 1, 2);
+        buttonsPanel.add(colsField, c);
+        setGBC(c, 0, 3);
+        buttonsPanel.add(totalMinesLabel, c);
+        setGBC(c, 1, 3);
+        buttonsPanel.add(totalMinesField, c);
+        // finally
+        wrapper.add(buttonsPanel, BorderLayout.NORTH);
+        return wrapper;
+    }
+
+    private void add_panels(){
+        add(difficultyPanel);
+    }
+
+    private void setGBC(GridBagConstraints gbc, int gridx, int gridy){
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+    }
+
+    private void setGBC(GridBagConstraints gbc, int gridx, int gridy, double weightx, double weighty){
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.weightx = weightx;
+        gbc.weighty = weighty;
+    }
+
+    private void setGBC(GridBagConstraints gbc, double weightx, double weighty){
+        gbc.weightx = weightx;
+        gbc.weighty = weighty;
     }
 }
