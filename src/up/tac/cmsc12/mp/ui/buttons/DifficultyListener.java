33845be97bb17340dec5de5855396754ac25c30b
ddrhckrzz <andreimartillo2000@gmail.com>
2024-05-10 23:20:19 +0800

    made the file for the actionlistener of the difficulty panel, will finish it up later

---
+++ src/up/tac/cmsc12/mp/ui/buttons/DifficultyListener.java
@@ -0,0 +1,13 @@
+package up.tac.cmsc12.mp.ui.buttons;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DifficultyListener implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO add logic
+    }
+
+}
