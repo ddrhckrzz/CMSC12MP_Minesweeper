922c22835ab234230240cb86847b9d0f65b7e714
ddrhckrzz <andreimartillo2000@gmail.com>
2024-05-23 22:48:40 +0800

    made the scorepanel transparent

--- src/up/tac/cmsc12/mp/ui/frames/ScorePanel.java
+++ src/up/tac/cmsc12/mp/ui/frames/ScorePanel.java
@@ -1,6 +1,9 @@
 package up.tac.cmsc12.mp.ui.frames;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
@@ -29,6 +32,11 @@ public class ScorePanel extends JPanel {
         scoreTable.setVisible(true);
         scoreTable.setEnabled(false);
         scoreTable.getTableHeader().setReorderingAllowed(false);
+        scoreTable.setOpaque(true);
+        scoreTable.setFillsViewportHeight(true);
+        scoreTable.setBackground(new Color(0, 0, 0, 0));
+        scoreTable.getTableHeader().setBackground(new Color(0, 0, 0, 0));
+        setBackground(new Color(0, 0, 0, 0));
     }
 
     private void init_layout() {
@@ -36,12 +44,15 @@ public class ScorePanel extends JPanel {
     }
 
     private void add_panels() {
-        add(new JScrollPane(scoreTable), BorderLayout.CENTER);
+        JScrollPane holder = new JScrollPane(scoreTable);
+        holder.setBackground(new Color(0, 0, 0, 0));
+        add(holder, BorderLayout.CENTER);
         add(makeBottomPanel(), BorderLayout.SOUTH);
     }
 
     private JPanel makeBottomPanel() {
         JPanel bottomPanel = new JPanel();
+        bottomPanel.setBackground(new Color(0,0,0,0));
         JLabel label = new JLabel("Switch Difficulty Leaderboard: ");
         String[] difficulty = {"BEGINNER", "INTERMEDIATE", "EXPERT", "MASTER", "LEGEND"};
         JComboBox<String> comboBox = new JComboBox<String>(difficulty);
@@ -50,6 +61,13 @@ public class ScorePanel extends JPanel {
             public void itemStateChanged(ItemEvent e) {
                 int index = comboBox.getSelectedIndex();
                 scoreTable.setModel(scores[index]);
+                repaint();
+            }
+        });
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repaint();
             }
         });
         bottomPanel.add(label);
